d78d5e98de842517344133c8ed44f15aa9eb4abc
HEEKWON <higun295@gmail.com>
2025-03-18 21:42:14 +0900

    add video

--- lec_3/main.py
+++ lec_3/main.py
@@ -1,7 +1,18 @@
 import cv2
+import numpy as np
+from matplotlib import pyplot as plt
 
-img = cv2.imread('Lenna.png')
+# 이미지 읽기 (Grayscale)
+img = cv2.imread('Lenna.png', cv2.IMREAD_GRAYSCALE)
 
-cv2.imshow('image', img)
-cv2.waitKey(0)
-cv2.destroyWindow()
+# 히스토그램 계산
+hist = cv2.calcHist([img], [0], None, [256], [0, 256])
+
+# 히스토그램 출력
+plt.figure()
+plt.title("Grayscale Histogram")
+plt.xlabel("Bins")
+plt.ylabel("# of Pixels")
+plt.plot(hist)
+plt.xlim([0, 256])
+plt.show()
